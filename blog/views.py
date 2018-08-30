725a5cba3b5b16e2def15c07265093e2a7e283bc
FU DoA <htg00713@163.com>
2018-08-30 16:26:03 +0800

    2018.8.30.1

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView
+from .models import *
+
+# Create your views here.
+
+class Index(ListView):
+    model = Article
+    template_name = 'index.html'
+    queryset = Article.objects.all().order_by('-id')  # 获取到全部文章并按编号降序排列。
+    paginate_by = 5
+
+def Detail (request,Articleid):
+    return (request,'detail.html')
--- project_blog/urls.py
+++ project_blog/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from blog import views as blog_view
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', blog_view.Index.as_view()),
+    path('^admin/', admin.site.urls),
+    path('^detail/', blog_view.Detail, name='detail')
+
 ]
